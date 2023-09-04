238097af68cf6fea07c080dd48ff43c25dc8ea21
joaof12 <142408309+joaof12@users.noreply.github.com>
2023-09-04 13:55:15 +0000

    operadores.

---
+++ aulas/aula02/operadores.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+
+
+
+  int multiplicação = 2 * 2;
+  int divisao = 10 / 2;
+  int diferença = 3 - 2;
+  int soma = 5 + 5;
+
+  printf("soma = %i\n", soma);
+  printf("soma = %i\n", ++soma); // soma = soma + 1
+  printf("soma = %i\n", soma);
+  printf("soma = %i\n", soma++); // soma = soma - 1 
+  
+  
+  return 0;
+}
